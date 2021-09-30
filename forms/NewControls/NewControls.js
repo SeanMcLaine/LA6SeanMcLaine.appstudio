fb9e5f9e93371fc748aa4eceb7e6be286aaba542
SeanMcLaine <skm60363@creighton.edu>
2021-09-29 23:28:17 -0500

    sdd
    we

--- forms/NewControls/NewControls.js
+++ forms/NewControls/NewControls.js
@@ -7,3 +7,27 @@ chbColor.onchange = function() {
     }
     NSB.MsgBox(choices);
 };
+
+
+/*Control 2*/
+
+drpPlaces.onclick = function(s) {
+    if (typeof(s) == "object") {
+        return;
+    }
+    NSB.MsgBox(s + " " + drpPlaces.selection);
+};
+
+/*Control 3*/
+
+
+jumboClimate.onclick = function() {
+    NSB.MsgBox("Buy eletric cars or else");
+};
+
+
+
+
+/*Control 4*/
+
+Progressbar1.value=60;
--- forms/NewControls4/NewControls4.js
+++
@@ -1,7 +0,0 @@
-
-drpPlaces.onclick = function(s) {
-    if (typeof(s) == "object") {
-        return;
-    }
-    NSB.MsgBox(s + " " + drpPlaces.selection);
-};
